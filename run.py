67bfa60646b02856cdb8fe34afc09be813151fd6
RedEyes04 <132729132+RedEyes04@users.noreply.github.com>
2024-10-15 12:43:52 +0800

    Update run.py

--- run.py
+++ run.py
@@ -7,6 +7,17 @@ from push_rss_update.send_email import send_emails
 import json
 import sys
 import os
+import requests
+
+url = 'https://www.redeyes.top/friend.json'
+response = requests.get(url, verify=False)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Failed to retrieve data: {response.status_code}")
+
 
 # 爬虫部分内容
 config = load_config("./conf.yaml")
